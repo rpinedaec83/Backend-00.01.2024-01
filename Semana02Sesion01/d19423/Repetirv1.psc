cad8c0f090ccc242a7d40b57a5f83e7b1b0242b8
PR2004 <ramoslirion2021@gmail.com>
2024-02-05 20:54:02 -0500

    Semana 3 sesion 1

--- Semana02Sesion01/d19423/Repetirv1.psc
+++
@@ -1,8 +0,0 @@
-Proceso Repetirv1
-	operacion = 0
-	Repetir
-		escribir "Escribe una opcion"
-		leer operacion
-	Hasta Que operacion == 5
-	
-FinProceso
---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,33 @@
+var dni = 43434343;
+
+console.log(dni);
+
+dni = 23232323;
+
+console.log(dni);
+let ce = 123123123
+console.log(ce);
+
+ce = 99999999
+
+console.log(ce);
+
+const PI = 3.14159
+
+console.log(PI)
+
+//PI = 3.1415987;
+//console.log(PI);
+
+let ce1 = "Hola ce"
+
+{
+    let ce = 123;
+}
+
+let nombreApellido = "Patrick Ramos" //Camel Case
+let nombre_apellido = "David Lopez" //Snake Case
+let NombreApellido = "Juan Perez" //Pascal Case
+//let nombre-apellido = "Nombre Apellido" Kebab Case (no funciona aqui)
+
+//console.log("Hola desde el archivo js")
