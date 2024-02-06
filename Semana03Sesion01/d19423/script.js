9f05decfb561f3e41b0a05ce885620e103bd1677
Roberto Pineda <rpinedaec83@gmail.com>
2024-02-05 20:49:49 -0500

    Clase 05022024 primera parte

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -1,4 +1,4 @@
-
+"use strict";
 var dni = 43434343;
 
 console.log(dni);
@@ -17,9 +17,20 @@ const PI = 3.14159
 
 console.log(PI)
 
+
+var dni = "hola dni"
+console.log(dni);
 //PI = 3.1415987;
 //console.log(PI);
 
+let ce1 = "Hola ce"
 
+{
+    let ce = 123;
+}
+
+let nombreApellido = "Roberto Pineda"
+let nombre_apellido = "David Lopez"
+let NombreApellido = "Juan Perez"
 
 //console.log("Hola desde el archivo js")
