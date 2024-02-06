e5572ccc990d1208d425ed3a9735cf4a9875f11b
Roberto Pineda <rpinedaec83@gmail.com>
2024-02-05 20:32:05 -0500

    Clase 05022024 primera parte

---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,25 @@
+
+var dni = 43434343;
+
+console.log(dni);
+
+dni = 23232323;
+
+console.log(dni);
+let ce = 123123123
+console.log(ce);
+
+ce = 99999999
+
+console.log(ce);
+
+const PI = 3.14159
+
+console.log(PI)
+
+//PI = 3.1415987;
+//console.log(PI);
+
+
+
+//console.log("Hola desde el archivo js")
