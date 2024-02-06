f26eaf720498ee3aeaed6e3e0a7ac7d23f74c9c9
PR2004 <ramoslirion2021@gmail.com>
2024-02-05 21:29:46 -0500

    Cambios3

---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,82 @@
+"use strict";
+var dni = 43434343;
+
+console.log(dni);
+
+dni = 23232323;
+
+console.log(dni);
+let ce = 123123123
+console.log(ce);
+
+ce = 99999999
+
+console.log(ce);
+
+const PI = 3.14159
+
+console.log(PI)
+
+
+var dni = "hola dni"
+console.log(dni);
+//PI = 3.1415987;
+//console.log(PI);
+
+let ce1 = "Hola ce"
+
+{
+    let ce = 123;
+}
+
+let nombreApellido = "Roberto Pineda"
+let nombre_apellido = "David Lopez"
+let NombreApellido = "Juan Perez"
+
+//console.log("Hola desde el archivo js")
+
+console.log(nombreApellido+nombre_apellido)
+
+let esCasado = false;
+
+let arrHobbies = [
+    "tocar bajo",
+    "aeromodlismo",
+    "gunpla"
+]
+
+console.log(arrHobbies);
+
+console.log(arrHobbies[1]);
+
+let arrDatos = [
+    true,
+    "palabra",
+    12,
+    0.88
+]
+
+console.log(arrDatos[3]);
+
+let objPersona1 = {
+    nombre: "Roberto",
+    apellido: "Pineda",
+    edad: 40,
+    estadoCivil: false
+};
+
+console.log(objPersona.nombre)
+
+let objPersona2 = {
+    nombre: "David",
+    apellido: "Lopez",
+    edad: 30,
+    estadoCivil: true
+};
+
+let arrPersonas = [objPersona1,objPersona2];
+
+console.log(arrPersonas[1].nombre);
+
+//let es para variables que cambian
+//const es para variables que nunca cambian
