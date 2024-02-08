e3d5a947a8f2e31ee67837034f712bcc2c27ff2a
Gonzalo <you@example.com>
2024-02-07 22:22:53 -0500

    primer ejercicio resuelto

---
+++ Hackaton03/sv72694759/js/script.js
@@ -0,0 +1,14 @@
+function ejecicio1(){
+    let numero = prompt("Escriba su numero");
+    let nroCaracteres = numero.length;
+    if(nroCaracteres==3){
+        alert("Si tiene 3 caracteres")
+    }
+    else{
+        alert("No tiene 3 caracteres");
+    }
+}
+function ejecicio2(){
+alert("Ejercicio2")
+
+}
