ee12146bb5ed38b9b5aded422660c3d14fcb4785
Gonzalo <you@example.com>
2024-02-08 18:15:32 -0500

    ejerc2

--- Hackaton03/sv72694759/js/script.js
+++ Hackaton03/sv72694759/js/script.js
@@ -8,28 +8,13 @@ function ejecicio1(){
         alert("No tiene 3 caracteres");
     }
 }
+
 function ejecicio2(){
-    const readline = require('readline');
-
-    // Crear una interfaz de lectura
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    
-    // Preguntar al usuario que ingrese un número
-    rl.question('Por favor, ingresa un número entero: '), (input) => {
-      // Convertir la entrada a un número entero
-      const numero = parseInt(input);
-    
-      // Verificar si el número es negativo
-      if (numero < 0) {
-        console.log('El número ingresado es negativo.');
-      } else if (numero === 0) {
-        console.log('El número ingresado es cero.');
-      } else {
-        console.log('El número ingresado es positivo.');
-      }
-
-}
+    let numero = prompt("Escriba su numero");
+    if(numero>0){
+        alert("El nùmero es positivo")
+    }
+    else{
+        alert("El nùmero es negativo");
+    }
 }
