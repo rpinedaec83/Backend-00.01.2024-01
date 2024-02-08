e584af6f89dca8948b5c693fad22ec3ccf9eae71
helmutpacheco <helmutinp@gmail.com>
2024-02-07 22:11:15 -0500

    first js function

---
+++ Hackaton03/SV41168130/script.js
@@ -0,0 +1,12 @@
+function ejercicio1(){
+    let numero = prompt("Escriba su número");
+    let nroCaracteres = numero.length;
+
+    if(parseInt(numero) % 2 == 0)  {
+        console.log("Es par");
+    }
+    else {
+        console.log("Es impar")
+    }
+        
+}
