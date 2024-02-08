02c1d97cdc51172076b11e962ac991554d659bac
Gonzalo <you@example.com>
2024-02-08 18:11:48 -0500

    ejercicio2

--- Hackaton03/sv72694759/js/script.js
+++ Hackaton03/sv72694759/js/script.js
@@ -9,6 +9,27 @@ function ejecicio1(){
     }
 }
 function ejecicio2(){
-alert("Ejercicio2")
+    const readline = require('readline');
 
+    // Crear una interfaz de lectura
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    
+    // Preguntar al usuario que ingrese un número
+    rl.question('Por favor, ingresa un número entero: '), (input) => {
+      // Convertir la entrada a un número entero
+      const numero = parseInt(input);
+    
+      // Verificar si el número es negativo
+      if (numero < 0) {
+        console.log('El número ingresado es negativo.');
+      } else if (numero === 0) {
+        console.log('El número ingresado es cero.');
+      } else {
+        console.log('El número ingresado es positivo.');
+      }
+
+}
 }
