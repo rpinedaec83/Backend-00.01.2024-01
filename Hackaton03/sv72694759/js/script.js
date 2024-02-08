e1a697af99c79f2a8b6c118c57ef8788195e545d
Gonzalo <you@example.com>
2024-02-08 18:43:55 -0500

    ejercicicio34

--- Hackaton03/sv72694759/js/script.js
+++ Hackaton03/sv72694759/js/script.js
@@ -33,7 +33,7 @@ function ejecicio4(){
     let numero1 = prompt("Escriba el primer numero");
     let numero2 = prompt("Escriba el segundo numero");
     let numero3 = prompt("Escriba el tercer numero");
-    const numerosOrdenados = [numero1, numero2, numero3].sort((a, b) => a - b);
+    const numeros = [numero1, numero2, numero3].sort((a, b) => a - b);
 
-    alert('Números de menor a mayor:', numerosOrdenados);
+    alert('Números de menor a mayor:', numeros);
 }
