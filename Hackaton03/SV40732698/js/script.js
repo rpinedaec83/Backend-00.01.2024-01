d30c14bddd6eb421cb5da291483396574b39840b
Omarlobog <olg2021@gmail.com>
2024-02-07 22:20:09 -0500

    carpeta SV40732698

---
+++ Hackaton03/SV40732698/js/script.js
@@ -0,0 +1,10 @@
+function ejecicio1(){
+    let numero = prompt("Escriba su numero");
+    let nroCaracteres = numero.length;
+    if(nroCaracteres==3){
+        alert("Si tiene 3 caracteres")
+    }
+    else{
+        alert("No tiene 3 caracteres");
+    }
+}
