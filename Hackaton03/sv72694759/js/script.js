6680e4d0c077c6412ba4de47cd6d0f4df2680af5
Gonzalo <you@example.com>
2024-02-10 16:12:36 -0500

    kkmbio16

--- Hackaton03/sv72694759/js/script.js
+++ Hackaton03/sv72694759/js/script.js
@@ -178,28 +178,30 @@ function ejecicio15(){
 
 function ejecicio16(){
     let numero = prompt("Ingrese un nùmero del 1 al 7: ");
-    if(numero===1){
+   switch(numero){
+    case 1:
         alert("El dìa es lunes");
+    break;
+    case 2:
+    alert("El dìa es martes");
+    break;
+    case 3:
+        alert("El dìa es miercoles");
+    break;
+    case 4:
+    alert("El dìa es jueves");
+    break;
+    case 5:
+    alert("El dìa es viernes");
+    break;
+    case 6:
+    alert("El dìa es sabado");
+    break;
+    case 7:
+    alert("El dìa es domingo");
+    break;
+    default:
+    alert("Ingrese un nùmero del 1 al 7");            
+    break;
     }
-    if(numero===2){
-        alert("El dìa es martes");
-    }
-    if(numero===3){
-        alert("El dìa es mièrcoles");
-    }
-    if(numero===4){
-        alert("El dìa es jueves");
-    }
-    if(numero===5){
-        alert("El dìa es viernes");
-    }
-    if(numero===6){
-        alert("El dìa es sàbado");
-    }
-    if(numero===7){
-        alert("El dìa es domingo");
-    }
-    if(numero>=8){
-        alert("Ingrese un nùmero entero del 1 al 7");       
-    }
-    }
+}
