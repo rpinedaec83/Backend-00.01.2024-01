c3a52530bf2d036cfa52069c2aaff09c5972b9e7
Omarlobog <olg2021@gmail.com>
2024-02-11 12:02:46 -0500

    avance 1

--- Hackaton03/SV40732698/js/script.js
+++ Hackaton03/SV40732698/js/script.js
@@ -8,3 +8,121 @@ function ejecicio1(){
         alert("No tiene 3 caracteres");
     }
 }
+function ejecicio2(){
+    prompt ("Ejercicio2")
+}
+function ejecicio3(){
+     prompt ("Ejercicio3")
+}   
+function ejecicio4(){
+    prompt ("Ejercicio4");
+}
+
+function ejecicio5(){
+    prompt ("Ejercicio5");
+}
+function ejecicio6(){
+    prompt ("Ejercicio6");
+}
+function ejecicio7(){
+    prompt ("Ejercicio7");
+}
+function ejecicio8(){
+    prompt ("Ejercicio8");
+}
+function ejecicio9(){
+    prompt ("Ejercicio9");
+}
+function ejecicio10(){
+    prompt ("Ejercicio10");
+}
+function ejecicio11(){
+    prompt ("Ejercicio11");
+}
+function ejecicio12(){
+    prompt ("Ejercicio12");
+}
+function ejecicio13(){
+    prompt ("Ejercicio13");
+}
+function ejecicio14(){
+    prompt ("Ejercicio14");
+}
+function ejecicio15(){
+    prompt ("Ejercicio15");
+}
+function ejecicio16(){
+    prompt ("Ejercicio16");
+}
+function ejecicio17(){
+    prompt ("Ejercicio17");
+}
+function ejecicio18(){
+    prompt ("Ejercicio18");
+}
+function ejecicio19(){
+    prompt ("Ejercicio19");
+}
+function ejecicio20(){
+    prompt ("Ejercicio20");
+}
+function ejecicio21(){
+    prompt ("Ejercicio21");
+}
+function ejecicio22(){
+    prompt ("Ejercicio22");
+}
+function ejecicio23(){
+    prompt ("Ejercicio23");
+}
+function ejecicio24(){
+    prompt ("Ejercicio24");
+}
+function ejecicio25(){
+    prompt ("Ejercicio25");
+}
+function ejecicio26(){
+    prompt ("Ejercicio26");
+}
+function ejecicio27(){
+    prompt ("Ejercicio27");
+}
+function ejecicio28(){
+    prompt ("Ejercicio28");
+}
+function ejecicio29(){
+    prompt ("Ejercicio29");
+}
+function ejecicio30(){
+    prompt ("Ejercicio30");
+}
+function ejecicio31(){
+    prompt ("Ejercicio31");
+}
+function ejecicio32(){
+    prompt ("Ejercicio32");
+}
+function ejecicio33(){
+    prompt ("Ejercicio33");
+}
+function ejecicio34(){
+    prompt ("Ejercicio34");
+}
+function ejecicio35(){
+    prompt ("Ejercicio35");
+}
+function ejecicio36(){
+    prompt ("Ejercicio36");
+}
+function ejecicio37(){
+    prompt ("Ejercicio37");
+}
+function ejecicio38(){
+    prompt ("Ejercicio38");
+}
+function ejecicio39(){
+    prompt ("Ejercicio39");
+}
+function ejecicio40(){
+    prompt ("Ejercicio40");
+}
