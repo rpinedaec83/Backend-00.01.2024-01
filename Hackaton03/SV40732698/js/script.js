53c6b74c182eb6b65b48e0d6e32d6a5e281acc4e
Omarlobog <olg2021@gmail.com>
2024-02-11 13:48:34 -0500

    Update 2

--- Hackaton03/SV40732698/js/script.js
+++ Hackaton03/SV40732698/js/script.js
@@ -1,4 +1,5 @@
 function ejecicio1(){
+    prompt ("Reconoce si un numero tiene 3 digitos (oprima Enter para continuar)");
     let numero = prompt("Escriba su numero");
     let nroCaracteres = numero.length;
     if(nroCaracteres==3){
@@ -9,120 +10,120 @@ function ejecicio1(){
     }
 }
 function ejecicio2(){
-    prompt ("Ejercicio2")
+    prompt ("Reconoce si un numero es negativo (oprima Enter para continuar)");
 }
 function ejecicio3(){
-     prompt ("Ejercicio3")
+     prompt ("Reconoce si un numero termina en 4 (oprima Enter para continuar)");
 }   
 function ejecicio4(){
-    prompt ("Ejercicio4");
+    prompt ("Ordena numeros de menor a mayor (oprima Enter para continuar)");
 }
 
 function ejecicio5(){
-    prompt ("Ejercicio5");
+    prompt ("Calcula el precio total con descuentos segun pedido (oprima Enter para continuar)");
 }
 function ejecicio6(){
-    prompt ("Ejercicio6");
+    prompt ("Calcula el pago semanal a un Empleado (oprima Enter para continuar)");
 }
 function ejecicio7(){
-    prompt ("Ejercicio7");
+    prompt ("Calcula el precio total segun la membresia del cliente (oprima Enter para continuar)");
 }
 function ejecicio8(){
-    prompt ("Ejercicio8");
+    prompt ("Determina si el alumno aprobó el año (oprima Enter para continuar)");
 }
 function ejecicio9(){
-    prompt ("Ejercicio9");
+    prompt ("Calcula el nuevo sueldo de un Empleado (oprima Enter para continuar)");
 }
 function ejecicio10(){
-    prompt ("Ejercicio10");
+    prompt ("Determina si un número es par o impar (oprima Enter para continuar)");
 }
 function ejecicio11(){
-    prompt ("Ejercicio11");
+    prompt ("Encuentra el mayor de 3 números (oprima Enter para continuar)");
 }
 function ejecicio12(){
-    prompt ("Ejercicio12");
+    prompt ("Encuentra el mayor de 2 números (oprima Enter para continuar)");
 }
 function ejecicio13(){
-    prompt ("Ejercicio13");
+    prompt ("Identifica una vocal (oprima Enter para continuar)");
 }
 function ejecicio14(){
-    prompt ("Ejercicio14");
+    prompt ("Identifica un numero primo del 1 al 10 (oprima Enter para continuar)");
 }
 function ejecicio15(){
-    prompt ("Ejercicio15");
+    prompt ("Convierta centímetros a pulgadas y libras a kilogramos (oprima Enter para continuar)");
 }
 function ejecicio16(){
-    prompt ("Ejercicio16");
+    prompt ("Encuentre el dia que corresponde a un número del 1 7 (oprima Enter para continuar)");
 }
 function ejecicio17(){
-    prompt ("Ejercicio17");
+    prompt ("Calcula la hora en un segundo despues (oprima Enter para continuar)");
 }
 function ejecicio18(){
-    prompt ("Ejercicio18");
+    prompt ("Calcule precio total y comision del vendedor (oprima Enter para continuar)");
 }
 function ejecicio19(){
-    prompt ("Ejercicio19");
+    prompt ("Pago semanal a empleados (oprima Enter para continuar)");
 }
 function ejecicio20(){
-    prompt ("Ejercicio20");
+    prompt ("Operaciones con 4 numeros enteros positivos (oprima Enter para continuar)");
 }
 function ejecicio21(){
-    prompt ("Ejercicio21");
+    prompt ("Calcule el factorial de un numero (oprima Enter para continuar)");
 }
 function ejecicio22(){
-    prompt ("Ejercicio22");
+    prompt ("Calcula la suma de n numeros (oprima Enter para continuar)");
 }
 function ejecicio23(){
-    prompt ("Ejercicio23");
+    prompt ("Calcula la suma de nuemros impares hasta un núnero n (oprima Enter para continuar)");
 }
 function ejecicio24(){
-    prompt ("Ejercicio24");
+    prompt ("Calcula la suma de números pares hasta 1000 (oprima Enter para continuar)");
 }
 function ejecicio25(){
-    prompt ("Ejercicio25");
+    prompt ("calcule el factorial de un número (oprima Enter para continuar)");
 }
 function ejecicio26(){
-    prompt ("Ejercicio26");
+    prompt ("Calcula el cociente y el resto en una división (oprima Enter para continuar)");
 }
 function ejecicio27(){
-    prompt ("Ejercicio27");
+    prompt ("Calcula la media hasta un numero n (oprima Enter para continuar)");
 }
 function ejecicio28(){
-    prompt ("Ejercicio28");
+    prompt ("Calcula la suma de los numeros del 1 al 100 (oprima Enter para continuar)");
 }
 function ejecicio29(){
-    prompt ("Ejercicio29");
+    prompt ("Calcula la suma de los numeros del 1 al 100 (oprima Enter para continuar)");
 }
 function ejecicio30(){
-    prompt ("Ejercicio30");
+    prompt ("Calcula la suma de los numeros del 1 al 100 (oprima Enter para continuar)");
 }
 function ejecicio31(){
-    prompt ("Ejercicio31");
+    prompt ("Calcula la media de 10 numeros, separadors en pares e impares (oprima Enter para continuar)");
 }
 function ejecicio32(){
-    prompt ("Ejercicio32");
+    prompt ("muestra la ciudad con máyor población (oprima Enter para continuar)");
 }
 function ejecicio33(){
-    prompt ("Ejercicio33");
+    prompt ("Consulta si desea continuar)");
 }
 function ejecicio34(){
-    prompt ("Ejercicio34");
+    prompt ("Tabla de multiplincar del 1 al 9 (oprima Enter para continuar)");
 }
 function ejecicio35(){
-    prompt ("Ejercicio35");
+    prompt ("Muestra el número mayor y menor de 20 numeros (oprima Enter para continuar)");
 }
 function ejecicio36(){
-    prompt ("Ejercicio36");
+    prompt ("Calcula la serie de Fibonnci (oprima Enter para continuar)");
 }
 function ejecicio37(){
-    prompt ("Ejercicio37");
+    prompt ("Calcula el M.C.D. (oprima Enter para continuar)");
 }
 function ejecicio38(){
-    prompt ("Ejercicio38");
+    prompt ("Identifica si un numero es perfecto (oprima Enter para continuar)");
 }
 function ejecicio39(){
-    prompt ("Ejercicio39");
+    prompt ("Aproximación a Pi por serie de Gregpry-Leibniz (oprima Enter para continuar)");
 }
 function ejecicio40(){
-    prompt ("Ejercicio40");
+    prompt ("Aproximación a Pi por serie de Nilakantha (oprima Enter para continuar)");
 }
