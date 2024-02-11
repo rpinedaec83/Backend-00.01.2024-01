d25a39595bea9dddc1c2847840e24d04f6de1f5e
Stefanno Rossel <stefannorp1510@gmail.com>
2024-02-10 19:34:00 -0500

    Ejercicios corregidos y aumentados hasta el 16

--- Hackaton03/SV72733562/ejercicio4.js
+++ Hackaton03/SV72733562/ejercicio4.js
@@ -11,13 +11,46 @@ numero3=parseInt(numero3)
 
 if (numero1> numero2 && numero1>numero3)
 {
-    console.log("El primer numero es el mayor");
+    console.log("El numero mayor es el " + numero1);
+    if(numero2>numero3)
+    {
+        console.log("El segundo numero es el " + numero2)
+        console.log("El tercer numero es el " + numero3)
+    }
+    else
+    {
+        console.log("El segundo numero es el " + numero3)
+        console.log("El tercer numero es el " + numero2)
+    }
+
 }
+
 else if (numero2> numero1 && numero2>numero3)
 {
-    console.log("El segundo numero es el mayor");
+    console.log("El numero mayor es el " + numero2);
+    if(numero1>numero3)
+    {
+        console.log("El segundo numero es el " + numero1)
+        console.log("El tercer numero es el " + numero3)
+    }
+    else
+    {
+        console.log("El segundo numero es el " + numero3)
+        console.log("El tercer numero es el " + numero1)
+    }
 }
 else if (numero3> numero1 && numero3>numero2)
 {
-    console.log("El tercer numero es el mayor");
+    console.log("El numero mayor es el "+ numero3);
+
+    if(numero2>numero1)
+    {
+        console.log("El segundo numero es el " + numero2)
+        console.log("El tercer numero es el " + numero1)
+    }
+    else
+    {
+        console.log("El segundo numero es el " + numero1)
+        console.log("El tercer numero es el " + numero2)
+    }
 }
