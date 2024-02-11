a756c45dd27279efbb68716b8cf084ec55b6a193
Gonzalo <you@example.com>
2024-02-10 20:08:11 -0500

    factorial

--- Hackaton03/sv72694759/js/script.js
+++ Hackaton03/sv72694759/js/script.js
@@ -404,3 +404,24 @@ function ejecicio24(){
     // Mostrar el resultado mediante alert
     alert(`La suma de todos los números pares hasta 1000 es: ${sumaPares}`);
 }
+
+function ejecicio25(){
+    let numero = parseInt(prompt("Ingrese un número para calcular su factorial:"));
+
+    // Validar si el número es un entero positivo
+    if (!isNaN(numero) && numero >= 0) {
+      // Calcular el factorial
+      let factorial = 1;
+    
+      for (let i = 1; i <= numero; i++) {
+        factorial *= i;
+      }
+    
+      // Mostrar el resultado mediante alert
+      alert(`El factorial de ${numero} es: ${factorial}`);
+    } else {
+      // Mostrar mensaje de error si el número no es válido
+      alert("Por favor, ingrese un número entero positivo.");
+    }
+
+}
