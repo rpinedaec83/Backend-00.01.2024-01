90e6fa8c1c281334997a25409a67eff8d3321f4e
Stefanno Rossel <stefannorp1510@gmail.com>
2024-02-11 18:53:47 -0500

    Ejercicios terminados

---
+++ Hackaton03/SV72733562/ejercicio37.js
@@ -0,0 +1,17 @@
+
+let primernumero = parseInt(prompt("Ingresa el primer número: "));
+let segundo = parseInt(prompt("Ingresa el segundo número: "));
+
+
+
+   
+    while (segundonumero !== 0) 
+    {
+        let nuevonumero = b;
+        b = a % b;
+        a = nuevonumero;
+    }
+
+    const mcd = a;
+
+    console.log("El MCD es: "+ mcd);
