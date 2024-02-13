a1a55e6b6f57609ee761210083ef5acb9c56af60
Roberto Pineda <rpinedaec83@gmail.com>
2024-02-12 19:50:12 -0500

    clase 12022024

---
+++ Semana04Sesion01/d19423/js/script.js
@@ -0,0 +1,18 @@
+
+debugger
+
+console.log("Inicio de la clase");
+
+function saludar(){
+    console.log("Hola");
+}
+
+function despedir(){
+    return "chao";
+}
+
+let msg = saludar();
+console.log(msg)
+let mensaje = despedir();
+
+console.log(mensaje)
