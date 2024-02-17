ad82d98851e7b4fa85277f728d68bd94d3fc4064
Gonzalo <you@example.com>
2024-02-17 17:11:32 -0500

    addd1

--- Hackaton04/SV72694759/js/script.js
+++ Hackaton04/SV72694759/js/script.js
@@ -1,6 +1,9 @@
 function ejercicio1(num1, num2) {
+    return num1 + num2;
+}
+
+// Ejemplo de uso:
 let resultado = ejercicio1(5, 3);
 console.log(resultado); // Esto imprimirá 8
-alert(resultado); // Esto imprimirá 8
-}
+
 
