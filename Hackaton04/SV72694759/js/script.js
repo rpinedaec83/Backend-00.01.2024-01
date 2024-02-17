18db56f421d57588cd961aabe384fc69613d5ca8
Gonzalo <you@example.com>
2024-02-17 17:05:16 -0500

    cambio

--- Hackaton04/SV72694759/js/script.js
+++ Hackaton04/SV72694759/js/script.js
@@ -1,8 +1,6 @@
 function ejercicio1(num1, num2) {
-    return num1 + num2;
-}
-
-// Ejemplo de uso:
 let resultado = ejercicio1(5, 3);
 console.log(resultado); // Esto imprimirá 8
 alert(resultado); // Esto imprimirá 8
+}
+
