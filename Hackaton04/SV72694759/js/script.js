4735e193952fd4be6ade2f321eec3d83b5b4d144
Gonzalo <you@example.com>
2024-02-17 17:19:48 -0500

    wqeqw

--- Hackaton04/SV72694759/js/script.js
+++ Hackaton04/SV72694759/js/script.js
@@ -1,11 +1,9 @@
-function ejercicio1(num1, num2) {
-    var suma = num1 + num2;
-    return suma;
-  var numero1 = parseFloat(prompt("Ingresa el primer número:"));
-  var numero2 = parseFloat(prompt("Ingresa el segundo número:"));
-  
-  var resultado = sumarNumeros(numero1, numero2);
-  
-  alert("La suma de los dos números es: " + resultado);
-  }
+function sumarNumeros(num1, num2) {
+    return num1 + num2;
+}
+
+// Ejemplo de uso:
+let resultado = sumarNumeros(5, 3);
+console.log(resultado); // Esto imprimirá 8
+
   
