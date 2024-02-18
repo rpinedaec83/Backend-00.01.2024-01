1d3b83456cde4cd8411bee2dfd815ba740ca9297
Alvaro Escalante <alvaro3882447@gmail.com>
2024-02-18 17:15:41 -0500

    04_Hackaton

--- Hackaton04/SV70860105/js/scripts.js
+++ Hackaton04/SV70860105/js/scripts.js
@@ -1,11 +1,15 @@
-
+//ESTAN COMENTADOS LOS EJEMPLOS DE CADA FUNCION
 //RETO 1
 
 const suma = (a, b) => {return a + b};
+//console.log(suma(7,8));
+
 
 const potencia = (num, pot) => {
     return Math.pow(num,pot);
 }
+//console.log(potencia(5,3));
+
 
 const suma_cubos = (...args) => {
     let suma = 0;
@@ -14,8 +18,12 @@ const suma_cubos = (...args) => {
     }
     return suma;
 }
+//console.log(suma_cubos(1,3,5));
+
 
 const area_triangulo = (base, altura) =>{ return base * altura / 2 }
+//console.log(area_triangulo(5,6));
+
 
 const operacion = (dato1, signo, dato2) =>{
     const signos = ['+', '-', 'x', '%'];
@@ -36,18 +44,26 @@ const operacion = (dato1, signo, dato2) =>{
         }
     }
     else{
-        return console.log("El parametro no es reconocido");
+        return "El parametro no es reconocido";
     }
 }
+//console.log(operacion(1,'y',4));
+//console.log(operacion(10,'x',6));
 
 
 //RETO 2
 
 const nombre_completo = (nombre, apellido, edad) => {return `Hola mi nombre es ${nombre + " " + apellido} y mi edad es ${edad}`}
+//console.log(nombre_completo("Alvaro", "Escalante", 24));
 
-const sumofcubes = (...args) => {return args.reduce((acumulador, num) => acumulador + Math.pow(num,3) , 0)};
+
+const sumOfCubes = (...args) => {return args.reduce((acumulador, num) => acumulador + Math.pow(num,3) , 0)};
+//console.log(sumOfCubes(1, 5, 9));
+
 
 const tipodato = (dato) => {return typeof dato}
+//console.log(tipodato("hola"));
+
 
 const sumanumeros = (...args) => {
     let suma = 0;
@@ -56,6 +72,8 @@ const sumanumeros = (...args) => {
     }
     return suma;
 }
+//console.log(sumanumeros(1,5,6,7,8,2));
+
 
 const nostrings = (lista) => {
     let no_strings = [];
@@ -66,19 +84,27 @@ const nostrings = (lista) => {
     }
     return no_strings;
 }
+//console.log(nostrings([2,5,"adrian"]));
+
 
 const min_max = (lista) =>{
+    let nums = [];
     lista.sort((a, b) => a - b);
     let menor= lista[0];
-
+    nums.push(menor);
     lista.sort((a, b) => b - a );
     let mayor = lista[0];
-    return console.log("Menor : " + menor + " Mayor :" + mayor);
+    nums.push(mayor);
+    return nums;
 }
+//console.log(min_max([57,8,9,35,265,98,1]));
 
-const tel_num = (lista) =>{
+
+const formatPhoneNumber = (lista) =>{
     return `(${lista[0] + "" + lista[1] + "" + lista[2]}) ${lista[3] + "" + lista[4] + "" + lista[5]}-${lista[6] + "" + lista[7] + "" + lista[8]}`;
 }
+//console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
 
 const findLargestNums = (listas) =>{
     let mayores = [];
@@ -88,6 +114,8 @@ const findLargestNums = (listas) =>{
     }
     return mayores;
 }
+//console.log(findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]));
+
 
 const charIndex = (palabra, letra)=>{
     let indices = [];
@@ -95,6 +123,8 @@ const charIndex = (palabra, letra)=>{
     indices.push(palabra.lastIndexOf(letra));
     return indices;
 }
+//console.log(charIndex("hello", "l"));
+
 
 const toArray = (objeto) => {
     let array = [];
@@ -107,6 +137,8 @@ const toArray = (objeto) => {
     }
     return array;
 }
+//console.log(toArray({ a: 1, b: 2 }));
+
 
 const getBudgets = (objetos) =>{
     let budgets = 0;
@@ -115,6 +147,8 @@ const getBudgets = (objetos) =>{
     }
     return budgets;
 }
+//console.log(getBudgets([{ name: "John", age: 21, budget: 23000 },{ name: "Steve",  age: 32, budget: 40000 },{ name: "Martin",  age: 16, budget: 2700 }]));
+
 
 const getStudentNames = (matriz) =>{
     let nombres = [];
@@ -123,6 +157,7 @@ const getStudentNames = (matriz) =>{
     }
     return nombres;
 }
+//console.log(getStudentNames([{ name: "Steve" },{ name: "Mike" },{ name: "John" }]));
 
 
 const objectToArray = (objeto) =>{
@@ -136,6 +171,8 @@ const objectToArray = (objeto) =>{
     }
     return array;
 }
+//console.log(objectToArray({likes: 2,dislikes: 3,followers: 10}));
+
 
 const squaresSum = (n) => {
     let sum = 0;
@@ -144,10 +181,14 @@ const squaresSum = (n) => {
     }
     return sum;
 }
+//console.log(squaresSum(3));
+
 
 const multiplyByLength = (matriz) =>{
     return matriz.map(x => x * matriz.length);
 }
+//console.log(multiplyByLength([2, 3, 1, 0]));
+
 
 const countdown = (num) =>{
     let matriz = [];
@@ -156,16 +197,54 @@ const countdown = (num) =>{
     }
     return matriz;
 }
+//console.log(countdown(5));
+
+const diffMaxMin = (lista) =>{
+
+    lista.sort((a, b) => a - b);
+    let menor= lista[0];
+    
+    lista.sort((a, b) => b - a );
+    let mayor = lista[0];
+   
+    return mayor - menor;
+}
+//console.log(diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]));
+
+
+const filterList = (matriz) => {
+    let enteros = [];
+    for( valor of matriz){
+        if(typeof(valor) == 'number'){
+            enteros.push(valor);
+        }
+    }
+    return enteros;
+}
+//console.log(filterList([1, 2, 3, "x", "y", 10]));
+
 
 const repeat = (num, veces) =>{
     return Array(veces).fill(num);
 }
+//console.log(repeat(13, 5));
+
+
+String.prototype.vreplace = function(nuevaVocal) {
+    return this.replace(/[aeiou]/g, nuevaVocal);
+};
+
+const palabra = "apples and bananas".vreplace("u");
+//console.log(palabra);
+
 
 const findNemo = (cadena) =>{
     let palabras = cadena.split(" ");
     console.log(palabras);
     return palabras.indexOf("Nemo") + 1;
 }
+//console.log("I found Nemo at " + findNemo("I am finding Nemo !"));
+
 
 const capLast = (palabra) =>{
     let letras = palabra.split("");
@@ -173,10 +252,6 @@ const capLast = (palabra) =>{
     let text = letras.join("");
     return text;
 }
+//console.log(capLast("hello"));
 
-String.prototype.vreplace = function(nuevaVocal) {
-    return this.replace(/[aeiou]/g, nuevaVocal);
-};
 
-const palabra = "apples and bananas".vreplace("u");
-console.log(palabra);
