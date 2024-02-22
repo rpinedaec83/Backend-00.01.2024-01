ee1208dd74662d7cec987bf4cf77f67e8e9e14e7
helmutpacheco <helmutinp@gmail.com>
2024-02-21 22:14:06 -0500

    add classes

--- Semana05Sesion02/SV41168130/js/script.js
+++ Semana05Sesion02/SV41168130/js/script.js
@@ -51,6 +51,81 @@ class Encuesta{
     }
 }
 
-function crearEncuestador{
-    
+let arrEncuestadores = [];
+
+var $table = $('#table');
+
+$table.bootstrapTable({data:arrEncuestadores})
+//nombre, apellido, fechaNacimiento, sexo, direccion, codigo, empresa
+function crearEncuestador(){
+      Swal.fire({
+        title: "Agregar Encuestadores",
+        html: `			
+        <div class="form-group">
+            <label for="name">Nombre</label>
+                <div class="input-group">
+                <input type="text" class="form-control" name="name" id="nombre"  placeholder="Ingresa el Nombre"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="name">Apellido</label>
+                <div class="input-group">
+                <input type="text" class="form-control" name="name" id="apellido"  placeholder="Ingresa el Apellido"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Fecha de Nacimiento</label>
+                <div class="input-group">
+                <input type="text" class="form-control" id="fechaNacimiento" name="fechaNacimiento" placeholder="Ingrese la fecha de nacimiento"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="username">Escala</label>
+                <div class="input-group">
+                   
+                    <input type="text" class="form-control" id="escala" name="username" placeholder="Ingresa la escala"/>
+                </div>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Tipo</label>
+                <div class="input-group">
+                   
+                    <input type="text" class="form-control" id="tipo" name="password" placeholder="Ingresa el Tipo"/>
+                </div>
+        </div>
+
+       
+
+
+        `,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `Salir`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            let txtnombre  = document.getElementById("nombre").value;
+            let txtdescripcion  = document.getElementById("descripcion").value;
+            let txtescala  = document.getElementById("escala").value;
+            let txttipo  = document.getElementById("tipo").value;
+            let objModelo={
+                nombre: txtnombre,
+                descripcion:txtdescripcion,
+                escala: txtescala,
+                tipo: txttipo
+            };
+            
+            arrModelos.push(objModelo);
+            console.log(arrModelos)
+            
+            $table.bootstrapTable('load', arrModelos)
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
 }
