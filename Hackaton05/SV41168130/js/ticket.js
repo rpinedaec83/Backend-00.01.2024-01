0e1c0442be797816c98d042b8f0576ee98605d2a
helmutpacheco <helmutinp@gmail.com>
2024-02-26 21:21:42 -0500

    adding hackaton not finished

---
+++ Hackaton05/SV41168130/js/ticket.js
@@ -0,0 +1,12 @@
+class Ticket{
+    constructor(state, description, phone, payment, paymentPercentage, client){
+        this.state = state;
+        this.description = description;
+        this.phone = phone;
+        this.payment = payment;
+        this.paymentPercentage = paymentPercentage;
+        this.client = client;
+    }
+}
+
+module.exports = Ticket;
--- Semana05Sesion02/SV41168130/js/script.js
+++ Semana05Sesion02/SV41168130/js/script.js
@@ -1,7 +1,7 @@
 class Persona {
-    constructor(nombre, apellido, fechaNacimiento, sexo, direccion){
-        this.nombre = nombre;
-        this.apellido = apellido;
+    constructor(nombres, apellidos, fechaNacimiento, sexo, direccion){
+        this.nombres = nombres;
+        this.apellidos = apellidos;
         this.fechaNacimiento = fechaNacimiento;
         this.sexo = sexo;
         this.direccion = direccion;
@@ -9,10 +9,9 @@ class Persona {
 }
 
 class Encuestador extends Persona {
-    constructor (nombre, apellido, fechaNacimiento, sexo, direccion, codigo, empresa){
-        super(nombre,apellido,fechaNacimiento, sexo, direccion);
+    constructor (nombres, apellidos, fechaNacimiento, sexo, direccion, codigo){
+        super(nombres,apellidos,fechaNacimiento, sexo, direccion);
         this.codigo = codigo;
-        this.empresa = empresa;
     }
 
     encuestar(encuestado, encuesta){
@@ -21,10 +20,9 @@ class Encuestador extends Persona {
 }
 
 class Encuestado extends Persona {
-    constructor(nombre, apellido, fechaNacimiento, sexo, direccion, nivelEducativo, nivelSocioEconomico){
-        super(nombre, apellido, fechaNacimiento, sexo, direccion);
+    constructor(nombres, apellidos, fechaNacimiento, sexo, direccion, nivelEducativo){
+        super(nombres, apellidos, fechaNacimiento, sexo, direccion);
         this.nivelEducativo = nivelEducativo;
-        this.nivelSocioEconomico = nivelSocioEconomico;
     }
 }
 
@@ -52,51 +50,61 @@ class Encuesta{
 }
 
 let arrEncuestadores = [];
+let arrEncuestados = [];
+let arrTipoPregunta = [];
+let arrPregunta = [];
+let arrEncuestas = [];
 
-var $table = $('#table');
+var $tableEncuestador = $('#tableEncuestador');
+$tableEncuestador.bootstrapTable()
 
-$table.bootstrapTable({data:arrEncuestadores})
-//nombre, apellido, fechaNacimiento, sexo, direccion, codigo, empresa
+var $tableEncuestado = $('#tableEncuestado');
+$tableEncuestado.bootstrapTable();
+
 function crearEncuestador(){
       Swal.fire({
-        title: "Agregar Encuestadores",
+        title: "Agregar Encuestador",
         html: `			
         <div class="form-group">
-            <label for="name">Nombre</label>
+            <label for="name">Nombres</label>
                 <div class="input-group">
-                <input type="text" class="form-control" name="name" id="nombre"  placeholder="Ingresa el Nombre"/>
+                <input type="text" class="form-control" name="name" id="nombres"  placeholder="Ingresa los Nombres"/>
             </div>
         </div>
         <div class="form-group">
-            <label for="name">Apellido</label>
+            <label for="apellidos">Apellidos</label>
                 <div class="input-group">
-                <input type="text" class="form-control" name="name" id="apellido"  placeholder="Ingresa el Apellido"/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="email">Fecha de Nacimiento</label>
-                <div class="input-group">
-                <input type="text" class="form-control" id="fechaNacimiento" name="fechaNacimiento" placeholder="Ingrese la fecha de nacimiento"/>
+                <input type="text" class="form-control" name="name" id="apellidos"  placeholder="Ingresa los Apellidos"/>
             </div>
         </div>
 
         <div class="form-group">
-            <label for="username">Escala</label>
+            <label for="fechaNacimiento">Fecha de Nacimiento</label>
                 <div class="input-group">
-                   
-                    <input type="text" class="form-control" id="escala" name="username" placeholder="Ingresa la escala"/>
+                <input type="date" class="form-control" id="fechaNacimiento" name="descripcion" placeholder="Ingrese la fecha de nacimiento"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="username">Sexo</label>
+                <div class="input-group">                   
+                    <input type="text" class="form-control" id="sexo" name="username" placeholder="Ingrese el sexo"/>
                 </div>
         </div>
 
         <div class="form-group">
-            <label for="password">Tipo</label>
-                <div class="input-group">
-                   
-                    <input type="text" class="form-control" id="tipo" name="password" placeholder="Ingresa el Tipo"/>
+            <label for="password">Direccion</label>
+                <div class="input-group">                   
+                    <input type="text" class="form-control" id="direccion" name="username" placeholder="Ingrese la direccion"/>
                 </div>
         </div>
 
+        <div class="form-group">
+            <label for="password">Codigo</label>
+                <div class="input-group">                   
+                    <input type="text" class="form-control" id="codigo" name="username" placeholder="Ingrese el codigo"/>
+                </div>
+        </div>
        
 
 
@@ -108,21 +116,217 @@ function crearEncuestador(){
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-            let txtnombre  = document.getElementById("nombre").value;
-            let txtdescripcion  = document.getElementById("descripcion").value;
-            let txtescala  = document.getElementById("escala").value;
-            let txttipo  = document.getElementById("tipo").value;
-            let objModelo={
-                nombre: txtnombre,
-                descripcion:txtdescripcion,
-                escala: txtescala,
-                tipo: txttipo
-            };
+            let nombres  = document.getElementById("nombres").value;
+            let apellidos  = document.getElementById("apellidos").value;
+            let fechaNacimiento  = document.getElementById("fechaNacimiento").value;
+            let sexo  = document.getElementById("sexo").value;
+            let direccion  = document.getElementById("direccion").value;
+            let codigo  = document.getElementById("codigo").value;
+            let objEncuestador = new Encuestador(nombres, apellidos, fechaNacimiento, sexo, direccion, codigo);
             
-            arrModelos.push(objModelo);
-            console.log(arrModelos)
+            arrEncuestadores.push(objEncuestador);
+            console.log(arrEncuestadores);
             
-            $table.bootstrapTable('load', arrModelos)
+            $tableEncuestador.bootstrapTable('load', arrEncuestadores);
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+}
+
+function crearEncuestado(){
+    Swal.fire({
+        title: 'Agregar Encuestado',
+        html: `
+        <div class = "form-group">
+            <label for="name">Nombres</label>
+            <div class="input-group">
+                <input type="text" class="form-control" name="name" id="nombres" placeholder="Ingrese los nombres"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="name">Apellidos</label>
+            <div class="input-group">
+                <input type="text" class="form-control" name="name" id="apellidos" placeholder="Ingrese los apellidos"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email">Fecha de Nacimiento</label>
+            <div class="input-group">                   
+                <input type="text" class="form-control" id="fechaNacimiento" name="descripcion" placeholder="Ingrese la fecha de nacimiento"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="username">Sexo</label>
+            <div class="input-group">                   
+                <input type="text" class="form-control" id="sexo" name="username" placeholder="Ingrese el sexo"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="username">Direccion</label>
+            <div class="input-group">               
+                <input type="text" class="form-control" id="direccion" name="username" placeholder="Ingrese la direccion"/>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label for="username">Codigo</label>
+            <div class="input-group">   
+                <input type="text" class="form-control" id="nivelEducativo" name="username" placeholder="Ingrese el nivel Educativo"/>
+            </div>
+        </div>
+        `,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `Salir`
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            let nombres  = document.getElementById("nombres").value;
+            let apellidos  = document.getElementById("apellidos").value;
+            let fechaNacimiento  = document.getElementById("fechaNacimiento").value;
+            let sexo  = document.getElementById("sexo").value;
+            let direccion  = document.getElementById("direccion").value;
+            let nivelEducativo  = document.getElementById("nivelEducativo").value;
+            let objEncuestado = new Encuestado(nombres,apellidos,fechaNacimiento,sexo,direccion,nivelEducativo)
+            arrEncuestados.push(objEncuestado);
+            console.log(arrEncuestados)
+            
+            $tableEncuestado.bootstrapTable('load', arrEncuestados)
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+}
+
+function crearTipoPregunta(){
+    Swal.fire({
+        title: "Agregar Encuestado",
+        html: `			
+        <div class="form-group">
+            <label for="name">Tipo Pregunta</label>
+            <div class="input-group">            
+                <input type="text" class="form-control" name="name" id="tipoPregunta"  placeholder="Ingrese el tipo de Pregunta"/>
+            </div>
+        </div>        
+        `,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `Salir`
+      }).then((result) => {
+           /* Read more about isConfirmed, isDenied below */
+           if (result.isConfirmed) {
+            let tipoPregunta  = document.getElementById("tipoPregunta").value;
+            let objTipoPregunta = new TipoPregunta(tipoPregunta)
+            arrTipoPregunta.push(objTipoPregunta);
+            console.log(arrTipoPregunta)
+            
+           // $tableEncuestado.bootstrapTable('load', arrEncuestados)
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+}
+
+function crearPregunta(){
+    
+    Swal.fire({
+        title: "Agregar Pregunta",
+        html: `			
+        <div class="form-group">
+            <label for="name">enunciado</label>
+                <div class="input-group">                
+                <input type="text" class="form-control" name="name" id="enunciado"  placeholder="Ingrese los Nombres"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="name">descripcion</label>
+                <div class="input-group">                
+                <input type="text" class="form-control" name="name" id="descripcion"  placeholder="Ingresa los Nombres"/>
+            </div>
+        </div>
+        <div class="form-group">
+        <label for="name">tipoPregunta</label>
+            <div class="input-group">
+                <select name="select">
+                    <option value="value1">Value 1</option>
+                    <option value="value2" selected>Value 2</option>
+                    <option value="value3">Value 3</option>
+                </select>
+                <input type="text" class="form-control" name="name" id="tipoPregunta"  placeholder="Ingresa los Nombres"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="name">opciones</label>
+            <div class="input-group">                
+                <input type="text" class="form-control" name="name" id="opciones"  placeholder="Ingresa los Nombres"/>
+            </div>
+        </div>
+        `,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `Salir`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            //enunciado, descripcion, tipoPregunta, opciones
+            let enunciado  = document.getElementById("enunciado").value;
+            let descripcion  = document.getElementById("descripcion").value;
+            let tipoPregunta  = document.getElementById("tipoPregunta").value;
+            let opciones  = document.getElementById("opciones").value;
+            let objPregunta = new Pregunta(enunciado,descripcion,tipoPregunta,opciones)
+            arrPregunta.push(objPregunta);
+            console.log(arrPregunta)
+            
+           // $tableEncuestado.bootstrapTable('load', arrEncuestados)
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+}
+
+function crearEncuesta(){
+    Swal.fire({
+        title: "Agregar Encuesta",
+        html: `			
+        <div class="form-group">
+            <label for="name">Fecha</label>
+            <div class="input-group">                
+                <input type="text" class="form-control" name="name" id="fecha"  placeholder="Ingresa los Nombres"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="name">Fecha</label>
+            <div class="input-group">
+                <select name="select">
+                    <option value="value1">Value 1</option>
+                    <option value="value2" selected>Value 2</option>
+                    <option value="value3">Value 3</option>
+                </select>
+            </div>
+        </div>        
+        `,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `Salir`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            let fecha  = document.getElementById("fecha").value;
+            let objEncuesta = new Encuesta(fecha,arrPregunta)
+            arrEncuestas.push(objEncuesta);
+            console.log(arrEncuestas)
+            
+           // $tableEncuestado.bootstrapTable('load', arrEncuestados)
           Swal.fire("Saved!", "", "success");
         } else if (result.isDenied) {
           Swal.fire("Changes are not saved", "", "info");
