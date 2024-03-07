e8bdc43fe793d98b57c288cfc28d285b7b9f905f
helmutpacheco <helmutinp@gmail.com>
2024-03-06 19:51:47 -0500

    sending finally files

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,6 +1,6 @@
 let http = require('http');
 
 http.createServer(function (req, res) {
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('<h1>Hola desde NodeJs!</h1>');
 }).listen(8080);
